81925f76c39bf9cb95613f73e7dc22da86fce718
Harrison Wu <harrisonywu@gmail.com>
2019-03-29 12:17:52 -0700

    Implement mongoose connection to the database

--- db/db.js
+++ db/db.js
@@ -1,8 +1,47 @@
 //set up mongoose here
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', {useNewUrlPaarser: true});
-
+mongoose.connect('mongodb://localhost/locations', {useNewUrlPaarser: true});
 var db = mongoose.connection;
+    
+var reviewSchema = new mongoose.Schema({
+  ID: Number,
+  Name: String,
+  Date: Date,
+  Avatar: String,
+  ReviewText: String,
+  RatingAccuracy: Number,
+  RatingCommunication: Number,
+  RatingCleanliness: Number,
+  RatingLocation: Number,
+  RatingCheckIn: Number,
+  RatingValue: Number
+})
+
+var locationSchema = new mongoose.Schema({
+  location: String,
+  reviews: [reviewSchema],
+})
+
+var Location = mongoose.model('location', locationSchema);
+Location.create({ 
+  location: 'Tokyo',
+  reviews: [{
+    ID: 1,
+    Name: 'Kevin',
+    Date: new Date().getMonth(),
+    Avatar: 'https://avatar.com',
+    ReviewText: 'blahblahblah',
+    RatingAccuracy: 1,
+    RatingCommunication: 2,
+    RatingCleanliness: 2,
+    RatingLocation: 2,
+    RatingCheckIn: 2,
+    RatingValue: 2
+  }]
+})
+
+console.log('hello')
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     //we're connected!
