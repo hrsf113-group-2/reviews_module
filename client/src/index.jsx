37c56dbf3acf624eb57c748eb6cc5c5fc6a1fe3f
Harrison Wu <harrisonywu@gmail.com>
2019-03-28 19:26:06 -0700

    Render react component in index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react';
+import ReactDOM from 'react-dom';
 
 // create a main component
 // but first start by making a shitty component that renders
 
-// class App extends React.Component {
-//   constructor() {
-//     super();
-//   }
+class App extends React.Component {
+  constructor() {
+    super();
+  }
 
-//   render() {
-//     return (
-//       <div>React Component App now rendering!</div>
-//     )
-//   }
-// }
+  render() {
+    return (
+      <div>React Component App now rendering!</div>
+    )
+  }
+}
 
-// ReactDOM.render(<App />, document.getElementById('app'))
-ReactDOM.render(<div>Sup bitches</div>, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'))
 
-// export default App;
+export default App;
