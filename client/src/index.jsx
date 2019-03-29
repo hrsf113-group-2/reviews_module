ef83d8883ba9d026dcf0b1de7b72354607ef794a
Harrison Wu <harrisonywu@gmail.com>
2019-03-28 19:23:01 -0700

    Set up server to run on port 1993

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,16 +4,19 @@ import ReactDOM from 'react';
 // create a main component
 // but first start by making a shitty component that renders
 
-class App extends React.Component {
-  constructor() {
-    super();
-  }
+// class App extends React.Component {
+//   constructor() {
+//     super();
+//   }
 
-  render() {
-    return (
-      <div>React Component App now rendering!</div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>React Component App now rendering!</div>
+//     )
+//   }
+// }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+// ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<div>Sup bitches</div>, document.getElementById('app'))
+
+// export default App;
