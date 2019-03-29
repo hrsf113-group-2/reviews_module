a2adff8cec3f88f0503a5e6dae97e91b75316fa3
Harrison Wu <harrisonywu@gmail.com>
2019-03-29 10:38:40 -0700

    Add build and server scripts, include mongoose as a dependency, and begin building database

---
+++ db/db.js
@@ -0,0 +1,10 @@
+//set up mongoose here
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlPaarser: true});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    //we're connected!
+})
+
