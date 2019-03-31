b1d4b89025dbe38cc864c4d5851d1bc47e2e8501
Harrison Wu <harrisonywu@gmail.com>
2019-03-31 15:08:25 -0700

    Refactor ratings to follow DRY convention, seed database with 100 entries instead of 10

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,15 @@
 const faker = require('faker');
 const Location = require('./db.js');
 
-for (let i = 1; i <= 10; i += 1) {
+for (let i = 1; i <= 100; i += 1) {
   const samplePosts = { locationID: i, reviews: [], };
   // plug in bottom 4 lines for 5starRatings but be sure the ratings are actually different each time.
-  var randomFiveStarRating = faker.random.number({
-    min: 1,
-    max: 5,
-  })
+  var createRandomFiveStarRating = function() {
+    return faker.random.number({
+      min: 1,
+      max: 5,
+    }); 
+  }
   
   // pick a random number between 1 and 20, then generate that many reviews
   var numberOfReviews = Math.floor(Math.random() * 20) + 1;
@@ -18,30 +20,12 @@ for (let i = 1; i <= 10; i += 1) {
       date: faker.date.recent(),
       avatar: faker.image.avatar(),
       text: faker.lorem.paragraph(),
-      "rating-accuracy": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
-      "rating-communication": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
-      "rating-cleanliness": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
-      "rating-location": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
-      "rating-check-in": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
-      "rating-value": faker.random.number({
-        min: 1,
-        max: 5,
-      }), 
+      "rating-accuracy": createRandomFiveStarRating(),
+      "rating-communication": createRandomFiveStarRating(),
+      "rating-cleanliness": createRandomFiveStarRating(),
+      "rating-location": createRandomFiveStarRating(),
+      "rating-check-in": createRandomFiveStarRating(),
+      "rating-value": createRandomFiveStarRating(),
     }
     samplePosts.reviews.push(review);
   }
