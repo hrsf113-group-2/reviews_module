f9ee49404e7c27e4b32efb95c85b67c44f31c91e
Harrison Wu <harrisonywu@gmail.com>
2019-04-03 18:00:10 -0700

    Remove comments from test file

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -16,30 +16,20 @@ describe('App Test Suite', () => {
     expect(wrapper.state('reviews')).toEqual([]);
   })
 
-  // test('Should render a div', () => {
-  //   const wrapper = shallow(<App />);
-  //   expect(wrapper.find(<div></div>)).to.have.length(1);
-  // })
-
-  // test('Test something related to component did mount here?', () => {
-
-  // })
-
-  // test('Test something related to rendering here', () => {
+  test('Should render a div', () => {
     
-  // })
+  })
+
+  test('Test something related to component did mount here?', () => {
+
+  })
+
+  test('Test something related to rendering here', () => {
+    
+  })
 
 
-  // test('Test something related to props here', () => {
+  test('Test something related to props here', () => {
 
-  // })
+  })
 });
-
-  
-  /* Tests to Add:
-    App
-    
-    1. componentDidMount (don't check for integratino)
-    2. does render something
-    3. render is of some number of Review components
-  */
