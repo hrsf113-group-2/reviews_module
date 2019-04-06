79c2c165b2e7a6a5096dada30975be0ebd856974
Harrison Wu <harrisonywu@gmail.com>
2019-04-05 18:04:15 -0700

    Fix comments made by Kris on pull request

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -21,22 +21,22 @@ describe('App Test Suite', () => {
   test('Reviews within state should initialize as an empty array', () => {
     const wrapper = shallow(<App />);
     expect(wrapper.state('allReviews')).toEqual([]);
-  })
+  });
 
   test('Should render a div', () => {
     
-  })
+  });
 
   test('Test something related to component did mount here?', () => {
 
-  })
+  });
 
   test('Test something related to rendering here', () => {
     
-  })
+  });
 
 
   test('Test something related to props here', () => {
 
-  })
+  });
 });
