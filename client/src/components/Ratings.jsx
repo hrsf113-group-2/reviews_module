cf26b8909a4eaea21cfc6e0196fd426d240f37a7
Harrison Wu <harrisonywu@gmail.com>
2019-04-05 21:21:03 -0700

    Render all ratings w/ appropriate heading, currently showing the rating as a number

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -23,7 +23,12 @@ Then refactor that into something reusable for each rating.
   render() {
     return (
       <div>
-        <div>{this.props.calculateAverageRating('rating-accuracy')}</div>
+        <div>Accuracy {this.props.calculateAverageRating('rating-accuracy')}</div>
+        <div>Communication {this.props.calculateAverageRating('rating-communication')}</div>
+        <div>Cleanliness {this.props.calculateAverageRating('rating-cleanliness')}</div>
+        <div>Location {this.props.calculateAverageRating('rating-location')}</div>
+        <div>Check-In {this.props.calculateAverageRating('rating-check-in')}</div>
+        <div>Value {this.props.calculateAverageRating('rating-value')}</div>
       </div>
     )
   }
