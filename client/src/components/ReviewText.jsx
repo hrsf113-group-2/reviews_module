05c18ef5debbc1ab85b05d7b4c9f982f64186d13
Harrison Wu <harrisonywu@gmail.com>
2019-04-06 18:47:31 -0700

    Add conditional rendering dependent on review text length

--- client/src/components/ReviewText.jsx
+++ client/src/components/ReviewText.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 
+/* 
+If the number of characters in this.props.reviewText < 250
+  Just render the review
+If not
+  Then for now render 'less than 250 characters'
+*/
+
 class ReviewText extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+
+    }
   }
 
   render() {
-    return (
-      <p>
-        {this.props.reviewText}
-      </p>
-    )
+    const currentReviewText = this.props.reviewText;
+    if (currentReviewText.length < 250) {
+      return (
+        <p>
+          {this.props.reviewText}
+        </p>
+      )
+    } else {
+      return (
+        <p>
+          This review has over 250 characters.
+        </p>
+      )
+    }
   }
 }
 
