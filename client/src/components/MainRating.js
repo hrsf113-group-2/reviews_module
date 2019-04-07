b396809a6baf3730fb4e4adf4f303d6acf751a2d
Harrison Wu <harrisonywu@gmail.com>
2019-04-06 17:58:41 -0700

    Calculate correct totalAverageRating in MainRating.js

--- client/src/components/MainRating.js
+++ client/src/components/MainRating.js
@@ -13,12 +13,27 @@ ALSO
 class MainRating extends React.Component {
   constructor(props) {
     super(props);
+
+    this.calculateTotalAverage = this.calculateTotalAverage.bind(this);
+  }
+
+  calculateTotalAverage(averageRatings) {
+    let ratingSum = 0;
+    let numOfKeys = 0;
+    for (let key in averageRatings) {
+      ratingSum += averageRatings[key];
+      numOfKeys += 1;
+    }
+    const totalAverage = ratingSum / numOfKeys;
+    const nearestHalfRating = Math.ceil(totalAverage*2)/2;
+    return nearestHalfRating;
   }
 
   render() {
     return (
       <div>
         <span>{this.props.numberOfReviews} Reviews</span>
+        <span>Total Average Rating: {this.calculateTotalAverage(this.props.allAverageRatings)}</span>
       </div>
     )
   }
