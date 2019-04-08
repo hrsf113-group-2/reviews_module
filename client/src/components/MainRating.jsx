bbdefc098f92096350dd2dc4200bf8c4e6f04525
Harrison Wu <harrisonywu@gmail.com>
2019-04-07 17:53:09 -0700

    Remove obsolete comments

--- client/src/components/MainRating.jsx
+++ client/src/components/MainRating.jsx
@@ -1,15 +1,4 @@
 import React from 'react';
-
-/* What should this component do?
-1. It should grab the ratings from App state.
-2. It should calculate an average of those, and go to the nearest .5 rating.
-3. It should render stars based on that rating
-
-ALSO
-1. it should count the number of reviews we have.
-
-
-*/
 class MainRating extends React.Component {
   constructor(props) {
     super(props);
