8cff77892fbe565c02d4555c61631a644b61d1b6
Harrison Wu <harrisonywu@gmail.com>
2019-04-07 17:25:48 -0700

    Render stars w/ correct percentage on the screen

--- client/src/components/Ratings.jsx
+++ client/src/components/Ratings.jsx
@@ -5,31 +5,16 @@ class Ratings extends React.Component {
     super(props);
   }
 
-// How the second class works
-/*
-10 = .5
-20 = 1
-30 = 1.5
-40 = 2
-50 = 2.5
-60 = 3
-70 = 3.5
-80 = 4
-90 = 4.5
-100 = 5
-*/
   createVisualStars(ratingValue) {
     const percentageOfStars = ratingValue * 20;
     const classNamePercentage = 'stars-' + percentageOfStars;
+    const classNames = 'full-star ' + classNamePercentage
     console.log(classNamePercentage)
     return (
-      <div>
         <div 
-          className='full-star'
-          className={classNamePercentage}>
+        className={classNames}>
           ★★★★★
         </div>
-      </div>
     )
   }
 
