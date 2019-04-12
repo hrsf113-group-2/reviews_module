10d6c5add27f1e945fd159fd42b24aa60e828c58
Harrison Wu <harrisonywu@gmail.com>
2019-04-11 23:00:33 -0700

    Create pagination component and have it rendering on screen

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Pagination extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Pagination Component now rendering!</div>
+    )
+  }
+}
+
+export default Pagination;
--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Review from './review';
+import Pagination from './Pagination'
 
 class ReviewsList extends React.Component {
   render() {
     return (
       <div>
         {this.props.currentReviews.map((review, index) => <Review key={index} review={review}/>)}
+        <Pagination />
       </div>
     );
   }
